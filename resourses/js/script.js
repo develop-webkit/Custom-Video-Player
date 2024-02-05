261d0236fdae02a8d594a420d1465f7cce7d1824
Nabeel Yaseen <nabeelyaseenggwp@gmail.com>
2024-02-06 02:39:06 +0500

    1.5

--- resourses/js/script.js
+++ resourses/js/script.js
@@ -1,10 +1,12 @@
+"use strict;"
+
 const videoEl = document.querySelector(".player__video");
+const progressBar = document.querySelector(".progress .progress__filled");
+const progressBarParent = document.querySelector(".progress");
 const playBtn = document.querySelector(".player__button.toggle");
 const volSlider = document.querySelector(".player__slider[name='volume']");
 const videoSpeed = document.querySelector(".player__slider[name='playbackRate']");
 const playBackSkips = document.querySelectorAll(".player__button[data-skip]");
-
-//console.log(playBackSkip[0].getAttribute('data-skip'))
 
 function videoPlay(){ 
     if(videoEl.paused){ 
@@ -22,15 +24,33 @@ function videoVolumeChange(){
 
 function videoSpeedChange(){
     videoEl.playbackRate = this.value;
-    console.log(this.value)
 }
+
+function progressBarChnage(){
+    progressBar.style.flexBasis = `${( this.currentTime / this.duration  ) * 100}%`;
+}
+
+function updatePrgressBar(e){
+    const offsets = progressBarParent.getBoundingClientRect();
+    const progressPer = ((e.clientX - offsets.left) / offsets.width) * 100;
+    progressBar.style.flexBasis = `${progressPer}%`;
+    videoEl.currentTime = ((progressPer / 100) * videoEl.duration);
+}
+
+
 
 
 playBtn.addEventListener('click', videoPlay);
 videoEl.addEventListener('click', videoPlay);
 volSlider.addEventListener('input', videoVolumeChange);
 videoSpeed.addEventListener('input', videoSpeedChange);
+videoEl.addEventListener('timeupdate',progressBarChnage);
+progressBarParent.addEventListener('click',updatePrgressBar)
 
-// for(const skip of playBackSkips){
-//     skip.addEventListener('click', () => { console.log(this)})   
-// }
+
+for(const skip of playBackSkips){
+    skip.addEventListener('click', (e) => { 
+        console.log(Number(videoEl.currentTime) + Number(e.target.getAttribute('data-skip')));
+        videoEl.currentTime = Number(videoEl.currentTime) + Number(e.target.getAttribute('data-skip'));
+    })   
+}
