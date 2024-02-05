4b708969803f5bc359e62401a2bf9ab8df91be55
Nabeel Yaseen <nabeelyaseenggwp@gmail.com>
2024-02-06 00:32:11 +0500

    1.3

--- resourses/js/script.js
+++ resourses/js/script.js
@@ -1,3 +1,36 @@
 const videoEl = document.querySelector(".player__video");
+const playBtn = document.querySelector(".player__button.toggle");
+const volSlider = document.querySelector(".player__slider[name='volume']");
+const videoSpeed = document.querySelector(".player__slider[name='playbackRate']");
+const playBackSkips = document.querySelectorAll(".player__button[data-skip]");
 
-videoEl.addEventListener('click', video);
+//console.log(playBackSkip[0].getAttribute('data-skip'))
+
+function videoPlay(){
+    if(videoEl.paused){
+        videoEl.play();
+        playBtn.innerHTML = "‖‖";
+    }else{
+        playBtn.innerHTML = "▶";
+        videoEl.pause();
+    }
+}
+
+function videoVolumeChange(){
+    videoEl.volume = this.value;
+}
+
+function videoSpeedChange(){
+    videoEl.playbackRate = this.value;
+    console.log(this.value)
+}
+
+
+playBtn.addEventListener('click', videoPlay);
+videoEl.addEventListener('click', videoPlay);
+volSlider.addEventListener('input', videoVolumeChange);
+videoSpeed.addEventListener('input', videoSpeedChange);
+
+// for(const skip of playBackSkips){
+//     skip.addEventListener('click', () => { console.log(this)})   
+// }
